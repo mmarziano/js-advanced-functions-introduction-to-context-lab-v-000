c8af3bcd75e7d55caa38260ce541cdc9065505e2
Monica Marziano <mmarziano1@gmail.com>
2019-10-24 11:50:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
 // Your code here
+
+function createEmployeeRecord(emp) {
+  const employee = {};
+  employee.firstName = emp[0];
+  employee.familyName = emp[1];
+  employee.title = emp[2];
+  employee.payPerHour = emp[3];
+  employee.timeInEvents = [];
+  employee.timeOutEvents = [];
+  return employee;
+}
+
+function createEmployeeRecords(emps) {
+  const employeeRecords = [];
+  emps.map(employee => employeeRecords.push(createEmployeeRecord(employee)))
+  return employeeRecords
+}
+
+function createTimeInEvent(emp, time) {
+  // let employee = createEmployeeRecord(emp);
+  let timeObj = {
+    type: "TimeIn",
+    hour: parseInt(time.split(' ')[1]),
+    date: time.split(' ')[0]
+  }
+  emp.timeInEvents.push(timeObj)
+  return emp
+}
+
+function createTimeOutEvent(emp, time) {
+  // let employee = createEmployeeRecord(emp);
+  let timeObj = {
+    type: "TimeOut",
+    hour: parseInt(time.split(' ')[1]),
+    date: time.split(' ')[0]
+  }
+  emp.timeOutEvents.push(timeObj)
+  return emp
+}
+
+function hoursWorkedOnDate(emp, date) {
+  let hoursWorked = 0;
+  for (let i = 0; i < date.length; i++) {
+    hoursWorked = hoursWorked + (emp.timeOutEvents[i].hour-emp.timeInEvents[i].hour)/100
+  }
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(emp, date) {
+  let hours = hoursWorkedOnDate(emp, date);
+  let pay = hours * emp.payPerHour;
+  return pay;
+}
+
+function allWagesFor(emp) {
+  let datesWorked = emp.timeInEvents.map(obj => obj.date);
+  let wages = 0;
+  for (let i = 0; i < datesWorked.length; i++) {
+    wages = wages + wagesEarnedOnDate(emp, datesWorked[i]);
+  }
+  return wages;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
